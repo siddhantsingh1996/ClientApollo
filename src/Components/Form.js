8623a5fe366e26eeea29f81a17b3258a87bbf356
unknown <siddhant.singh5120@yahoo.in>
2021-09-09 18:00:04 +0530

    removed multiple names

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -50,13 +50,7 @@ console.log("logo",todo,client);
   };
   return (
     <div>
-      <input
-        type="text"
-        placeholder="First Name"
-        onChange={(e) => {
-          setFirstName(e.target.value);
-        }}
-      />
+      
       <input
         type="text"
         placeholder="Last Name"
